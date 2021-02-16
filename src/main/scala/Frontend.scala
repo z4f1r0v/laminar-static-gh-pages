4f85fad87cdb4b5652a1165787cc45009914aa19
z4f1r0v <alexander@zafirov.me>
2021-02-16 11:55:30 +0100

    Modify to look like Kit's demo

--- src/main/scala/Frontend.scala
+++ src/main/scala/Frontend.scala
@@ -1,27 +1,59 @@
 import com.raquo.laminar.api.L._
 import org.scalajs.dom
 
+import scala.util.Random
+
+case class Rectangle(width: Double, height: Double, color: Color)
+
+case class Color(r: Int, g: Int, b: Int) {
+  override def toString() = s"rgb($r, $g, $b)"
+}
+
 object Frontend {
 
   val myApp = {
 
-    val $tick = EventStream.periodic(1000)
+    def $randomRectangle: EventStream[Rectangle] =
+      EventStream.periodic(1000).mapTo(Rectangle.random)
 
     div(
-      div(
-        "Tick #: ",
-        child.text <-- $tick.map(_.toString)
+      display.flex,
+      span(
+        opacity(0.7),
+        "You rectangle is: ",
+        marginRight("8px")
       ),
-      div(
-        "Random #: ",
-        child.text <-- $tick.mapTo((scala.util.Random.nextInt() % 100).toString)
-      )
+        RectangleView($randomRectangle),
+      marginRight("8px"),
+        RectangleView($randomRectangle),
+      marginRight("8px"),
+      RectangleView($randomRectangle),
     )
   }
+
+  def RectangleView($rectangle: EventStream[Rectangle]): Div =
+    div(
+      width <-- $rectangle.map(r => s"${r.width}px"),
+      height <-- $rectangle.map(r => s"${r.height}px"),
+      borderRadius("2px"),
+      background <-- $rectangle.map(_.color.toString())
+    )
 
   def main(args: Array[String]): Unit = {
     documentEvents.onDomContentLoaded.foreach { _ =>
       render(dom.document.getElementById("appContainer"), myApp)
     }(unsafeWindowOwner)
   }
+}
+
+object Rectangle {
+  val empty = Rectangle(0.0, 0.0, Color.random)
+
+  def random = {
+    Rectangle(Random.nextDouble() * 30, Random.nextDouble() * 30, Color.random)
+  }
+}
+
+object Color {
+  def random: Color = Color(Random.nextInt, Random.nextInt, Random.nextInt)
 }
